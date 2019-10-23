6587fad8a7ab723916e094d43c5b420d70500709
hmp31 <55255926+hmp31@users.noreply.github.com>
2019-10-23 14:30:20 -0600

    Update testdata.sql

--- testdata.sql
+++ testdata.sql
@@ -3,3 +3,9 @@ INSERT INTO users VALUES ('admin123', 'pwd12345', 'a', 'John', 'Doe', 'Edmonton'
 
 INSERT INTO persons VALUES ('Jill', 'Jackson', '1989-10-10', 'Edmonton', '1912 Main St', '234-514-8549');
 INSERT INTO users VALUES ('JJack123', 'Sec56789', 'o', 'John', 'Doe', 'Edmonton');
+
+INSERT INTO persons VALUES ('Tina', 'McGreggor','1995-07-08', 'Calgary', '29108 291a Ave', '986-685-4254');
+INSERT INTO users VALUES ('TM1', 'pwd12345', 'a', 'Tina', 'McGreggor', 'Calgary');
+
+INSERT INTO persons VALUES ('Milton', 'Watt', '1979-10-23', 'St Albert', '4768 Apple Ave', '707-856-8397');
+INSERT INTO users VALUES ('admin123', 'pwd12345', 'a', 'Milton', 'Watt', 'Edmonton');
