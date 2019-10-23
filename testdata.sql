8808abc778760e507b8de2e1229a6adbef39f52c
Djohnronan <dronan@ualberta.ca>
2019-10-23 12:09:32 -0600

    added Jill Jackson as ticket officer to db; made testdata file

---
+++ testdata.sql
@@ -0,0 +1,5 @@
+INSERT INTO persons VALUES ('John', 'Doe', '1985-04-03', 'Edmonton', '123 YEG St', '404-444-4404');
+INSERT INTO users VALUES ('admin123', 'pwd12345', 'a', 'John', 'Doe', 'Edmonton');
+
+INSERT INTO persons VALUES ('Jill', 'Jackson', '1989-10-10', 'Edmonton', '1912 Main St', '234-514-8549');
+INSERT INTO users VALUES ('JJack123', 'Sec56789', 'o', 'John', 'Doe', 'Edmonton');
